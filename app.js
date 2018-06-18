5178cb5faf1713ef7a4e865c9a00b114edcf0b75
Hampton Moore <me@hampton.pw>
2018-06-18 10:13:37 -0400

    Update

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 var yaml = require('write-yaml');
 var express = require('express');
 var readYaml = require('js-yaml');
-var exec = require('child_process').exec, child;
+var exec = require('child_process').exec,
+    child;
 var fs = require('fs');
 var functions = require('./functions.js');
 var apps = {};
 var server = express();
 var compose = null;
-var bodyParser = require('body-parser')
- 
+var bodyParser = require('body-parser');
 
-function loadApps(){
-    fs.readdirSync("./applications").forEach(file => {
-      apps[file.replace(".json", "")] = require("./applications/"+file);
-      console.log("Loaded "+ file)
-    })
+
+function loadApps() {
+    fs.readdirSync("./applications").forEach(function (file) {
+        apps[file.replace(".json", "")] = require("./applications/" + file);
+        console.log("Loaded " + file);
+    });
 }
 
 loadApps();
@@ -22,12 +23,12 @@ var data = {};
 
 try {
     data = require("./data.json");
-} catch(err){
+} catch (err) {
     console.log("Save not found, Creating");
     fs.writeFileSync('./data.json', JSON.stringify({}));
 }
 
-console.log(apps)
+console.log(apps);
 
 /*fs.readFile('./docker-compose.yml', "utf8", function (err, data) {
     if (err == "Error: ENOENT: no such file or directory, open './docker-compose.yml'"){
@@ -60,66 +61,74 @@ try {
 }
 
 
-var launchApp = function(name, app, vars){
-    data[name] = {app: app, vars: vars, date: Math.floor(Date.now() / 1000)};
+var launchApp = function (name, app, vars) {
+    data[name] = {
+        app: app,
+        vars: vars,
+        date: Math.floor(Date.now() / 1000)
+    };
     compose.services[name] = functions.generateApp(apps[app]["docker-compose"], vars);
-    /*exec('docker-compose up -d '+name,
+    exec('docker-compose up -d '+name,
     function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
         if (error !== null) {
              console.log('exec error: ' + error);
         }
-    });*/
+    });
 };
 
-yaml('docker-compose.yml', compose, function(err) {
-    if (err){throw err;}
+yaml('docker-compose.yml', compose, function (err) {
+    if (err) {
+        throw err;
+    }
 });
 
 fs.writeFileSync('./data.json', JSON.stringify(data));
 
-server.use(function(req, res, next) {
+server.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
-server.use( bodyParser.json() );       // to support JSON-encoded bodies
-server.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+server.use(bodyParser.json()); // to support JSON-encoded bodies
+server.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
 }));
 
-server.post('/api/create', function(req, res) {
-    
+server.post('/api/create', function (req, res) {
+
     var newApp = JSON.parse(req.body.data);
-    
+
     var newAppVars = JSON.parse(JSON.stringify(newApp));
-    
+
     delete newAppVars.id;
     delete newAppVars.name;
-    
+
     console.log(newApp);
-    
+
     launchApp(newApp.name, newApp.id, newAppVars);
-    res.send(newApp.name+" "+ newApp.id)
-    
-    yaml('docker-compose.yml', compose, function(err) {
-        if (err){throw err;}
+    res.send(newApp.name + " " + newApp.id);
+
+    yaml('docker-compose.yml', compose, function (err) {
+        if (err) {
+            throw err;
+        }
     });
-    
+
     fs.writeFileSync('./data.json', JSON.stringify(data));
 });
 
-server.get("/api/apps.json", function(req, res){
+server.get("/api/apps.json", function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(apps));
 });
 
-server.use('/', express.static('serve'))
+server.use('/', express.static('serve'));
 
-server.listen(8080, function(){
+server.listen(8082, function () {
     console.log("Server Started");
 });
 
-setTimeout(loadApps, 1000*60);
+setTimeout(loadApps, 1000 * 60);
